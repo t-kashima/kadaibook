21774f096c19ca8c04cc0db46c0fef4fe7212969
Kashima Takumi <kodokurider@gmail.com>
2011-12-19 13:48:08 +0900

    layout

--- app.rb
+++ app.rb
@@ -21,11 +21,15 @@ end
 get '/' do
   @book
   @error
-  if @amazonurl = params[:amazonurl] then
-    @book = Book.search(@amazonurl)
-    @book[:date] = (@book[:date] =~ /^(\d{4})/ ? $1 : "")
-    if Kadai::Library.search(@book[:isbn]).size > 0 then
-      @error =  "Library has the book"
+  @amazonurl = params[:amazonurl] || ""
+  if not @amazonurl.empty? then
+    @book = Book.search(@amazonurl) || ""
+    if @book.empty? then
+      @error = "Not a book"
+    elsif Kadai::Library.search(@book[:isbn]).size > 0 then
+      @error =  "This library has the book"
+    else
+      @book[:date] = (@book[:date] =~ /^(\d{4})/ ? $1 : "")
     end
   end
   haml :booksearch
